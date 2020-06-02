d66bf2258239eb11d68df39be9f97630af81a9c6
xiaoshane0408 <63327406+xiaoshane0408@users.noreply.github.com>
2020-06-02 11:18:11 +0800

    完成並發布網頁版

--- 3DEat/Assets/Script/GameManager.cs
+++ 3DEat/Assets/Script/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI;               // UI API
+using UnityEngine.SceneManagement;  // 場景管理 API
 
 public class GameManager : MonoBehaviour
 {
@@ -46,7 +47,7 @@ public class GameManager : MonoBehaviour
         for (int i = 0; i < total; i++)
         {
             // 座標 = (隨機, 1.5 , 隨機)
-            Vector3 pos = new Vector3(Random.Range(-9, 9), 1.5f, Random.Range(-9, 9));
+            Vector3 pos = new Vector3(Random.Range(-9, 9), 1, Random.Range(-9, 9));
             // 生成(物件, 座標, 角度)
             Instantiate(prop, pos, Quaternion.identity);
         }
@@ -59,21 +60,101 @@ public class GameManager : MonoBehaviour
     /// </summary>
     private void CountTime()
     {
+        // 如果取得所有雞腿 就 跳出
+        if (countProp == countTotal) return;
+
         // 遊戲時間 遞減 一幀的時間
         gameTime -= Time.deltaTime;
+
+        // 遊戲時間 = 數學.夾住(遊戲時間, 最小值, 最大值)
+        gameTime = Mathf.Clamp(gameTime, 0, 100);
+
         // 更新倒數時間介面 ToString("f小數點位數")
         textTime.text = "倒數時間：" + gameTime.ToString("f2");
+        
+        Lose();
+    }
+
+    /// <summary>
+    /// 取得道具
+    /// </summary>
+    public void GetProp(string prop)
+    {
+        if(prop == "雞腿")
+        {
+            countProp++;
+            textCount.text = "道具數量：" + countProp + " / " + countTotal;
+
+            Win();
+        }
+        else if(prop == "酒")
+        {
+            gameTime -= 2;
+            textTime.text = "倒數時間：" + gameTime.ToString("f2");
+        }
+    }
+
+    /// <summary>
+    /// 勝利：吃光所有雞腿
+    /// </summary>
+    private void Win()
+    {
+        // 如果雞腿數 = 雞腿總數
+        if(countProp == countTotal)
+        {
+            // 顯示結束畫面、啟動互動、啟動遮擋
+            final.alpha = 1;
+            final.interactable = true;
+            final.blocksRaycasts = true;
+            // 更新結束畫面標題
+            textTitle.text = "恭喜體重增加10公斤";
+        }
+    }
+
+    /// <summary>
+    /// 失敗：時間歸零
+    /// </summary>
+    private void Lose()
+    {
+        if(gameTime == 0)
+        {
+            // 顯示結束畫面、啟動互動、啟動遮擋
+            final.alpha = 1;
+            final.interactable = true;
+            final.blocksRaycasts = true;
+            // 更新結束畫面標題
+            textTitle.text = "吃太慢了吧";
+            // 取得玩家.啟動 = false
+            FindObjectOfType<Player>().enabled = false;
+        }
+    }
+
+    /// <summary>
+    /// 重置遊戲
+    /// </summary>
+    public void Replay()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void Quit()
+    {
+        // 應用程式.離開()
+        Application.Quit();
     }
     #endregion
 
     #region 事件
     private void Start()
     {
-        countTotal = CreateProp(props[0], 20);  // 道具總數 = 生成道具(道具一號, 指定數量)
+        countTotal = CreateProp(props[0], 6);  // 道具總數 = 生成道具(道具一號, 指定數量)
 
         textCount.text = "道具數量＂ 0 / " + countTotal;
 
-        CreateProp(props[1], 10);   // 生成道具(道具二號, 指定數量)
+        CreateProp(props[1], 5);   // 生成道具(道具二號, 指定數量)
     }
 
     private void Update()
--- 3DEat/Assets/Script/Player.cs
+++ 3DEat/Assets/Script/Player.cs
@@ -20,8 +20,11 @@ public class Player : MonoBehaviour
         }
     }
 
-    private Rigidbody rig;
-    private Animator ani;
+    private Rigidbody rig;   // 剛體
+    private Animator ani;    // 動畫
+    private AudioSource aud; // 喇叭
+    private GameManager gm;  // 遊戲管理器
+
     /// <summary>
     /// 旋轉角度
     /// </summary>
@@ -30,6 +33,12 @@ public class Player : MonoBehaviour
     /// 跳躍力道：從 0 慢慢增加
     /// </summary>
     private float jump;
+
+    [Header("雞腿音效")]
+    public AudioClip soundChicken;
+    [Header("酒音效")]
+    public AudioClip soundWine;
+
     #endregion
 
     #region 方法
@@ -97,9 +106,21 @@ public class Player : MonoBehaviour
     /// <summary>
     /// 碰到道具：碰到帶有標籤 "雞腿" 的物件
     /// </summary>
-    public void HitProp()
+    public void HitProp(GameObject prop)
     {
-
+        if(prop.tag == "雞腿")
+        {
+            // 喇叭.播放一次音效(音效片段, 音量)
+            aud.PlayOneShot(soundChicken, 1.2f);
+        }
+        else if(prop.tag == "酒")
+        {
+            aud.PlayOneShot(soundWine, 1.2f);
+        }
+        // 刪除(物件)
+        Destroy(prop);
+        // 告知 GM 取得道具(將道具標籤傳過去)
+        gm.GetProp(prop.tag);
     }
     #endregion
 
@@ -110,6 +131,10 @@ public class Player : MonoBehaviour
         // 剛體 = 取得元件<剛體>();
         rig = GetComponent<Rigidbody>();
         ani = GetComponent<Animator>();
+        aud = GetComponent<AudioSource>();
+        // FOOT 僅限於場僅上只有一個類別存在時使用
+        // 例如：場景上只有一個 GameManager 類別時可以使用它來取得
+        gm = FindObjectOfType<GameManager>();
     }
 
     // 固定更新頻率事件：1秒50幀，使用物理必須在此事件內
@@ -121,6 +146,40 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Jump();        
+    }
+
+    /* 碰撞事件 (沒有勾選 Is Trigger)
+    // 碰撞事件：當物件碰撞開始時執行一次 (沒有勾選 Is Trigger)
+    // collision 碰到物件的碰撞資訊
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+    // 碰撞事件：當物件碰撞離開時執行一次 (沒有勾選 Is Trigger)
+    private void OnCollisionExit(Collision collision)
+    {
+        
+    }
+    // 碰撞事件：當物件碰撞開始時持續執行 (沒有勾選 Is Trigger) 60FPS
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }*/
+
+    // 觸發事件：當物件碰撞開始時執行一次 (有勾選 Is Trigger)
+    private void OnTriggerEnter(Collider other)
+    {
+        HitProp(other.gameObject);
+    }
+    // 觸發事件：當物件碰撞離開時執行一次 (有勾選 Is Trigger)
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
+    // 觸發事件：當物件碰撞開始時持續執行 (有勾選 Is Trigger) 60FPS
+    private void OnTriggerStay(Collider other)
+    {
+        
     }
     #endregion
 }
